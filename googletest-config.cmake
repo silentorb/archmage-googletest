db41fe2da72b54554a8dfc0ba01dc1ecebde1302
cj <cj@silentorb.com>
2017-06-26 00:17:10 -0600

    Building for both mingw and msvc is now working.  ExternalProject_Add turns out to be actually useful when it truly is an external project and not one I am working on.

--- googletest-config.cmake
+++ googletest-config.cmake
@@ -1,5 +1,1 @@
-get_filename_component(PARENT_DIR "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY)
-get_filename_component(PARENT_DIR "${PARENT_DIR}" DIRECTORY)
-include(${PARENT_DIR}/scripts/include.cmake)
-include_boost()
-add_boost_library_path(filesystem)
+# TODO: Include libraries and header files here with switches to support differences between compilers.
