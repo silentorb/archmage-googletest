82c722d67df91902f0b05a9a3c02d536e154475c
cj <christopherwjohnson@gmail.com>
2017-10-24 12:17:11 -0600

    Fixed a bug where the code for copying the dlls was pointing to the wrong source folder.

--- googletest-config.cmake
+++ googletest-config.cmake
@@ -10,13 +10,14 @@ else ()
 endif ()
 
 set(lib_dir "${CMAKE_CURRENT_LIST_DIR}/bin/${GENERATOR_KEY}/lib")
+set(bin_dir "${CMAKE_CURRENT_LIST_DIR}/bin/${GENERATOR_KEY}/bin")
 include_directories(${CMAKE_CURRENT_LIST_DIR}/bin/${GENERATOR_KEY}/include)
 target_link_libraries(${CURRENT_TARGET} "${lib_dir}/${gtest_library}")
 target_link_libraries(${CURRENT_TARGET} "${lib_dir}/${gtest_main}")
 
 if (MSVC) # Compiled shared.
   add_custom_command(TARGET ${CURRENT_TARGET} POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy ${lib_dir}/gtest.dll $<TARGET_FILE_DIR:${CURRENT_TARGET}>
-    COMMAND ${CMAKE_COMMAND} -E copy ${lib_dir}/gtest_main.dll $<TARGET_FILE_DIR:${CURRENT_TARGET}>
+    COMMAND ${CMAKE_COMMAND} -E copy ${bin_dir}/gtest.dll $<TARGET_FILE_DIR:${CURRENT_TARGET}>
+    COMMAND ${CMAKE_COMMAND} -E copy ${bin_dir}/gtest_main.dll $<TARGET_FILE_DIR:${CURRENT_TARGET}>
     )
 endif ()
